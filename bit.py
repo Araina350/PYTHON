5e8be09f5064de2ce0693ee1d55412a09b37f563
Araina350 <aishwarya.agarwal20@gmail.com>
2026-04-06 12:14:39 +0530

    initial commit

---
+++ bit.py
@@ -0,0 +1,13 @@
+num1 = 10
+num2  = 67
+print("num1 and num2 ", num1 & num2)
+print("\nnum1 | num2 = ", num1 | num2)
+print("\n~num1", ~num1)
+print("\nnum1 ^ num2", num1 ^ num2)
+
+print("\nnum1 >> num2 = ", num1 >> 1)
+print("\nnum2 >> num1 = ", num2 >> 1)
+
+
+print("\nnum1 << num2 = ", num1 << 1)
+print("\nnum2 << num1 = ", num2 << 1)
